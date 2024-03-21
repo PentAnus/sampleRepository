cae0c033ecd85dee79182a5e6f2e8b8b39a58d6f
PentAnus <pentakoti.anusha@gmail.com>
2024-03-21 10:19:01 +0530

    Committed Msg

---
+++ FirstCode.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World!");
+		int a=1,b=2,c=3,d=5;
+		System.out.println(a++);
+		System.out.println(b++);
+		System.out.println(c--);
+		System.out.println(--d);
+	}
+
+}
